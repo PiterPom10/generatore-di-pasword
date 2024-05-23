a4f6bc540d7f877ae55e70b7c945afffd7157034
PiterPom10 <pietropomata2010@gmail.com>
2024-05-23 18:12:09 +0200

    Add files via upload

---
+++ M1L2.py
@@ -0,0 +1,15 @@
+
+import random
+elementi = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+lunghezza = int(input("inserire la lunghezza"))
+
+password_autogenerata = ""
+
+
+for i in range(lunghezza):
+    carattere_casuale = random.choice(elementi)
+    password_autogenerata = carattere_casuale
+
+    print(password_autogenerata)
+
